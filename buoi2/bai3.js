fa475d231e36f6f9fc45dbc4ff06eb9f89ebb735
HuongNTN <huongntn@payme.vn>
2023-09-14 21:21:31 +0700

    done bài 3

--- buoi2/bai3.js
+++ buoi2/bai3.js
@@ -1,34 +1,45 @@
-const url = 'https://jsonplaceholder.typicode.com/uses';
+const url = 'https://jsonplaceholder.typicode.com/users';
 const urlPost = 'https://jsonplaceholder.typicode.com/posts?userId=';
 // 1.
 const p = new Promise((resolve, reject) => {
   //fetch(url).then()
 })
 // Promise chaining
-fetch(url).then(res => {
-  if (res.status == 200) return res.json()
-  else {
-    console.log(res.status);
-    throw Error('Lỗi lấy thông tin user');
-  }
-})
-  .then((data) => {
-    // chon 1 user tu list
-    const index = Math.floor(Math.random() * 10);
-    const user = data[index];
-    console.log(`Lấy thông tin post của user: ${user.id}`);
-
-    fetch(urlPost + user.id).then((res) => {
-      if (res.status == 200) return res.json()
-      else {
-        console.log(res.status);
-        throw new Error('Lỗi lấy thông tin post');
-      }
-    }).then(posts => {
-      //In danh sách bài viết ra màn hình.
+const getUser = () => {
+  return fetch(url).then(res => {
+    if (res.status == 200) return res.json()
+    else {
+      console.log(res.status);
+      throw Error('Lỗi lấy thông tin user');
+    }
+  })
+    .then((data) => {
+      // chon 1 user tu list
+      const index = Math.floor(Math.random() * 10);
+      const user = data[index];
+      console.log(`Lấy thông tin post của user: ${user.id}`);
+      return user;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+const getPost = (userId) => {
+  return fetch(urlPost + userId).then((res) => {
+    if (res.status == 200) return res.json()
+    else {
+      console.log(res.status);
+      throw new Error('Lỗi lấy thông tin post');
+    }
+  }).then(posts => {
+    return posts
+  }).catch()
+}
+getUser()
+  .then((user) => {
+    // console.log(user);
+    //  in ds ra man hinh
+    getPost(user.id).then(posts => {
       posts.forEach((post) => console.log(`${post.id} - ${post.title}`));
     })
-  })
-  .catch((err) => {
-    console.log(err.message);
   });
