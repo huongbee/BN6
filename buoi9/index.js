7fb7385907cfebbeccf9569ccae238851aab6af8
HuongNTN <huongntn@payme.vn>
2023-10-01 10:29:15 +0700

    add loop

--- buoi9/index.js
+++ buoi9/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
+const users = require('./data/users.json');
 
 app.get('/users', (req, res) => {
-  res.render('list-user');
+  res.render('list-user', { users });
 });
 
 app.listen(3000, () => { console.log('server running on port 3000'); });
