bcb3e61ab520a5083ac432317b81649a36f46ed4
HuongNTN <huongntn@payme.vn>
2023-10-01 10:43:04 +0700

    add detail user

--- buoi9/index.js
+++ buoi9/index.js
@@ -8,4 +8,10 @@ app.get('/users', (req, res) => {
   res.render('list-user', { users });
 });
 
+app.get('/user/:id', (req, res) => {
+  // const id = +req.params.id;
+  const user = users.find(user => user.id == +req.params.id);
+  res.render('detail-user', { user });
+});
+
 app.listen(3000, () => { console.log('server running on port 3000'); });
