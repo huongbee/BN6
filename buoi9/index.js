5402fb0c23c0dd94c7e64aef746bce087161452a
HuongNTN <huongntn@payme.vn>
2023-10-02 19:40:13 +0700

    update

--- buoi9/index.js
+++ buoi9/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 const fs = require('fs');
 const { User } = require('./model/User');
-let { listUser } = require('./model/User'); //
+let { listUser } = require('./model/User');
 
 app.get('/users', (req, res) => {
   res.render('list-user', { users: listUser });
@@ -25,4 +27,15 @@ app.get('/update/:id', (req, res) => {
   const user = listUser.find(user => user.id == +req.params.id);
   res.render('update', { user });
 });
+app.post('/update', (req, res) => {
+  const { idUser, username, fullname, password } = req.body;
+  const user = listUser.find(user => user.id == idUser);
+  user.fullname = fullname;
+  user.password = password;
+  user.username = username;
+  res.redirect('/users');
+});
 app.listen(3000, () => { console.log('server running on port 3000'); });
+
+
+// http://localhost:3000/update?username=sale&password=111111&fullname=Sale+01
