fee21532636c8da1937654a3a55289e7126b0242
HuongNTN <huongntn@payme.vn>
2023-10-03 19:11:12 +0700

    insert

--- buoi11/index.js
+++ buoi11/index.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+const md5 = require('md5');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -7,3 +9,16 @@ const connection = mysql.createConnection({
   console.log('Error connecting to MySQL');
   console.log('Error code ' + err.code);
 })
+
+// insert
+connection.execute(
+  'INSERT INTO `Users` (`fullname`, `username`, `password`) VALUES (?,?,?)',
+  ['John Henry', 'john.h', md5('111111')],
+  (err, results) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log('Inserted userId = ' + results.insertId);
+    }
+  }
+)
