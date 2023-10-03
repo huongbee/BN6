de81b6271a27634dadcc14678d05857f2e9415a1
HuongNTN <huongntn@payme.vn>
2023-10-03 19:35:40 +0700

    select

--- buoi11/index.js
+++ buoi11/index.js
@@ -11,14 +11,41 @@ const connection = mysql.createConnection({
 })
 
 // insert
-connection.execute(
-  'INSERT INTO `Users` (`fullname`, `username`, `password`) VALUES (?,?,?)',
-  ['John Henry', 'john.h', md5('111111')],
-  (err, results) => {
-    if (err) {
-      console.log(err.message);
-    } else {
-      console.log('Inserted userId = ' + results.insertId);
-    }
-  }
-)
+// connection.execute(
+//   'INSERT INTO Users (fullname, username, password) VALUES (?,?,?)',
+//   ['Trần Thị Diễm Phúc', 'phuc.ttd', md5('111111')],
+//   (err, results) => {
+//     if (err) {
+//       console.log(err.message);
+//     } else {
+//       console.log('Inserted userId = ' + results.insertId);
+//     }
+//   }
+// );
+// connection.execute(
+//   'UPDATE Users SET fullname = ?, username = ? WHERE id = ?',
+//   ['Trần Văn Bình', 'binh.tv', 5],
+//   (err, results) => {
+//     if (err) {
+//       console.log(err.message);
+//     } else {
+//       console.log('ChangedRows = ' + results.changedRows);
+//     }
+//   }
+// );
+// connection.execute(
+//   'DELETE FROM Users WHERE id = ?',
+//   [5],
+//   (err, results) => {
+//     if (err) {
+//       console.log(err.message);
+//     } else {
+//       console.log(results);
+//     }
+//   }
+// );
+connection.query('SELECT * FROM users', (err, results, fields) => {
+  if (err) console.log(err.message);
+  console.log(results);
+  // console.log(fields);
+})
