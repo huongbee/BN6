e9d62e5501d927a20a598bf4a7696ae21b1ce88c
HuongNTN <huongntn@payme.vn>
2023-11-12 11:17:45 +0700

    buoi26

--- mailer/index.js
+++ mailer/index.js
@@ -12,7 +12,8 @@ const mailOptions = {
   console.log(r);
 })();
 
-
-// https://console.cloud.google.com tạo OAuth client ID
-// https://developers.google.com/oauthplayground tạo Refresh Token
-// https://mail.google.com // Authorize APIs
+//b1. https://console.cloud.google.com tạo OAuth client ID
+//b2. https://developers.google.com/oauthplayground tạo Refresh Token
+//nhập https://mail.google.com // Authorize APIs
+//chọn button Exchange authorization code for tokens, click auto f5
+//chọn icon configuration bên góc phải, nhập CLIENT_ID và CLIENT_SECRET, nhấn close
